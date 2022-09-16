2d02825b11ef2f3feb66bf0c4d41f06f7a71eaa2
karstand09 <karstand9@gmail.com>
2022-09-16 04:14:43 +0300

    ex_003 done

--- ex_003/Program.cs
+++ ex_003/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите ваше число: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}
