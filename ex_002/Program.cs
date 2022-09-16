202d60e8b7eef4e23773bc5dc1bc97f5d9bd1d77
karstand09 <karstand9@gmail.com>
2022-09-16 04:08:58 +0300

    ex_002 done

--- ex_002/Program.cs
+++ ex_002/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int Max = Math.Max(a, Math.Max(b, c));
+Console.WriteLine(Max);
