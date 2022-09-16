990cd6139cc6976a26a90eeb1c560a8d7f62ef35
karstand09 <karstand9@gmail.com>
2022-09-16 17:10:09 +0300

    ex_001 modified

--- ex_001/Program.cs
+++ ex_001/Program.cs
@@ -5,9 +5,15 @@ int a = int.Parse(Console.ReadLine());
 
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
+if(a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    int Max = Math.Max(a, b);
+    int Min = Math.Min(a, b);
 
-int Max = Math.Max(a, b);
-int Min = Math.Min(a, b);
-
-Console.WriteLine($"Большее число: {Max}");
-Console.WriteLine($"Меньшее число: {Min}");
+    Console.WriteLine($"Большее число: {Max}");
+    Console.WriteLine($"Меньшее число: {Min}");
+}
