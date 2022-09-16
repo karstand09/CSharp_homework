e0c0cb03328c4f4652fa7cd731c22d709723e7a3
karstand09 <karstand9@gmail.com>
2022-09-16 04:31:53 +0300

    ex_001 переделан, не правильно прочитал условие задачи

--- ex_001/Program.cs
+++ ex_001/Program.cs
@@ -1,5 +1,13 @@
 ﻿Console.Clear();
-Console.Write("Введите ваше число: ");
+
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
-int result = a * a;
-Console.WriteLine(result);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int Max = Math.Max(a, b);
+int Min = Math.Min(a, b);
+
+Console.WriteLine($"Большее число: {Max}");
+Console.WriteLine($"Меньшее число: {Min}");
