96c210e27f1ed6bce1ed09e54822568b23aadbac
karstand09 <karstand9@gmail.com>
2022-10-03 12:06:21 +0600

    new rename csproj

--- ex_010/Program.cs
+++ ex_010/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("Введите трехзначное число.\nДля выхода из программы введите комунду exit.");
-    while(true)
-    {
-        string num = Console.ReadLine();
-        if(!num.Equals("exit"))
-            Console.WriteLine("{0}->{1}",num, num[1]);
-        else
-            break;
-    }
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число.\nДля выхода из программы введите комунду exit.");
+
+while(true)
+{
+    string num = Console.ReadLine();
+    if(!num.Equals("exit"))
+        Console.WriteLine("{0}->{1}",num, num[1]);
+    else
+        break;
+}
