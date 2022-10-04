c3837a62725f738bbeee4060cba6932b7a9b2d9b
karstand09 <karstand9@gmail.com>
2022-10-04 23:38:00 +0600

    ex_023 done

--- ex_023/Program.cs
+++ ex_023/Program.cs
@@ -1,3 +1,13 @@
 ﻿//Задача 23
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+Console.Clear();
+
+Console.Write("Введите n: ");
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n ; i++)
+{
+    Console.Write((Math.Pow(i, 3) + " "));
+}
