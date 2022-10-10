b4f7a75fbb49446422fe209da9ff9fa0e719206d
karstand09 <karstand9@gmail.com>
2022-10-11 00:08:34 +0600

    ex_052 done

--- ex_052/Program.cs
+++ ex_052/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+double[,] matrix = new double[,] 
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+};
+
+void PrintArray(double[,] matr) // функция вывода массива
+{
+    for (int row = 0; row < matr.GetLength(0); row++)
+    {
+        for (int col = 0; col < matr.GetLength(1); col++)
+            {
+                Console.Write($"{matr[row, col]} ");
+            }
+        Console.WriteLine();
+    }
+}
+
+void columnAverage(double[,] matr) // функция нахождения среднего арифметического по столбцам
+{   
+    for (int row = 0; row < matrix.GetLength(1); row++)
+    {
+    double average = 0;
+    for (int col = 0; col < matrix.GetLength(0); col++)
+    {
+        average = average + matrix[col, row];
+    }
+    Console.Write(Math.Round(average / matrix.GetLength(0), 1));
+    Console.Write(" ");
+    }
+}
+
+PrintArray(matrix);
+Console.WriteLine();
+columnAverage(matrix);
