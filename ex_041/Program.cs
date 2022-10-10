db7da57db9046baaa6489b9293e9f56af516b4c2
karstand09 <karstand9@gmail.com>
2022-10-10 21:33:50 +0600

    ex_041 done

--- ex_041/Program.cs
+++ ex_041/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void findPositive()
+{
+    Console.WriteLine("\nEnter your numbers: ");
+    string? nums_str = Console.ReadLine();
+
+    int count = 0;
+
+    char[] separators = { ' ', ',', ';' };
+    string[] str_arr = nums_str.Split(separators);
+
+    for (int i = 0; i < str_arr.Length; i++)
+    {
+        if (Convert.ToInt32(str_arr[i]) > 0) { count++; };
+    }
+    Console.WriteLine("\nPositive numbers: " + count);
+}
+
+findPositive();
