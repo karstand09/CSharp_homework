3f68a49a001b31777d4031b9c3e5933023a27af0
karstand09 <karstand9@gmail.com>
2022-10-11 22:55:27 +0600

    ex_054 done

--- ex_054/Program.cs
+++ ex_054/Program.cs
@@ -8,4 +8,49 @@
 9 5 3 2
 8 4 4 2 */
 
+
 Console.Clear();
+
+int[,] matrix = new int[,] // создаём двумерный массив
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+};
+
+void PrintArray(int[,] matr) // функция вывода двумерного массива
+{
+    for (int row = 0; row < matr.GetLength(0); row++)
+    {
+        for (int col = 0; col < matr.GetLength(1); col++)
+            {
+                Console.Write($"{matr[row, col]} ");
+            }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[,] matr) // функция сортировки двумерного массива
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      for (int k = 0; k < matr.GetLength(1) - 1; k++)
+      {
+        if (matr[i, k] < matr[i, k + 1])
+        {
+          int temp = matr[i, k + 1];
+          matr[i, k + 1] = matr[i, k];
+          matr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+PrintArray(matrix);
+Console.WriteLine();
+SortMatrix(matrix);
+PrintArray(matrix);
